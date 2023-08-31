67e3ab6e605e435bc5f9e4b70b794bba3d2a3473
CesarIgnacio <cesarignac@gmail.com>
2023-08-31 18:42:47 -0400

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,8 @@ module.exports = {
     // Or if using `src` directory:
     './src/**/*.{js,ts,jsx,tsx,mdx}',
 
-    './node_modules/flowbite-react/**/*.{js,ts,jsx,tsx,mdx}'
+    './node_modules/flowbite-react/**/*.{js,ts,jsx,tsx,mdx}',
+    './public/**/*.html',
   ],
   darkMode: 'class',
   theme: {
