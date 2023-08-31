a3b7d71f40e4ad4a77d7dbab7e1bf90dd799f963
CesarIgnacio <cesarignac@gmail.com>
2023-08-31 12:47:24 -0400

    sidebar menu template

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,16 +1,18 @@
-"use client";
+import { MenuOption } from "./MenuOption";
+import { YearFolder } from "./YearFolder";
 
 export default function Sidebar() {
   return (
-    <div className="flex flex-col p-12">
-      <h1 className="">Ella Hepner</h1>
-      <span>Home</span>
-      <span>Drawings</span>
-      <span>Paintings</span>
-      <span>Prints</span>
-      <span>Installations</span>
-      <span>CV</span>
-      <span>Contact</span>
+    <div className="w-72 bg-white flex flex-col p-14 items-start">
+      <button className="text-2xl font-bold cursor-pointer">Ella Hepner</button>
+      <button className="text-base my-6 cursor-pointer">Home</button>
+      <MenuOption name="Paintings" />
+      <YearFolder name="2023" />
+      <YearFolder name="2022" />
+      <MenuOption name="Prints" />
+      <MenuOption name="Installations" />
+      <MenuOption name="CV" />
+      <MenuOption name="Contact" />
     </div>
   );
 }
